3fb7ae761b726010b63c89e6479dff43815c232f
Urban Eriksson <123ube@gmail.com>
2018-10-29 12:55:04 +0100

    KDtree refined

--- KDtree.m
+++ KDtree.m
@@ -1,60 +1,108 @@
 classdef KDtree
-    % KDtree  
-    
-    properties
-        root
+    % KDtree
+    % Assumes input where number of points >> dimensions
+    % i.e. low dimensional data
+
+    properties(Access=private)
+        root, dimensions, pre, post
     end
     
     methods
         % Constructor
         function obj = KDtree(pointarray)
-            dimensions = size(pointarray,2);
-            obj.root = KDtree.create_tree(pointarray,1, dimensions);
+            if size(pointarray,2) > size(pointarray,1)
+                obj.pre = @(x) x';
+                obj.post = @(x) x';
+            else
+                obj.pre = @(x) x;
+                obj.post = @(x) x;
+            end
+            pointarray = obj.pre(pointarray);
+            [points, obj.dimensions] = size(pointarray);
+            indexarray = (1:points)';
+            obj.root = KDtree.create_tree(pointarray, indexarray, 1, obj.dimensions);
         end
         
         function neighbors = neighborhood(obj, point, radius)
+            point = obj.pre(point);
             radius2 = radius^2;
-            dimensions = length(point);
-            neighbors = KDtree.get_neighborhood(obj.root, point, radius2, 1, dimensions);
-        end
-        
-        function neighbor = nearestneighbor(obj, point)
-            dimensions = length(point);
-            neighbor = KDtree.find_nearest_neighbor(obj.root, point, 1, dimensions,[], inf);
+            neighbors = KDtree.get_neighborhood(obj.root, point, radius2, 1, obj.dimensions);
+            neighbors = obj.post(neighbors);
         end
 
+        function indices = neighborhood_indices(obj, point, radius)
+            point = obj.pre(point);
+            radius2 = radius^2;
+            [~, indices] = KDtree.get_neighborhood_indices(obj.root, point, radius2, 1, obj.dimensions);
+            indices = obj.post(indices);
+        end
         
+        function nearestpoint = nearestneighbor(obj, point)
+            point = obj.pre(point);
+            nearestpoint = KDtree.find_nearest_node(obj.root, point, 1, obj.dimensions,[],-1, inf);
+            nearestpoint = obj.post(nearestpoint);
+        end
+        
+        function nearestindex = nearestneighbor_index(obj, point)
+             point = obj.pre(point);
+            [~, nearestindex] = KDtree.find_nearest_node(obj.root, point, 1, obj.dimensions,[],-1, inf);
+            nearestindex = obj.post(nearestindex);
+        end
+        
+        function points_in_range = rangesearch(obj, lowerlimit, upperlimit)
+            lowerlimit = obj.pre(lowerlimit);
+            upperlimit = obj.pre(upperlimit);
+            centerpoint = (upperlimit + lowerlimit) / 2;
+            halfrange = (upperlimit - lowerlimit) / 2;
+            points_in_range = KDtree.perform_range_search(obj.root, centerpoint, halfrange, 1, obj.dimensions);
+            points_in_range = obj.post(points_in_range);
+        end
+
+        function indices_in_range = rangesearch_indices(obj, lowerlimit, upperlimit)
+            lowerlimit = obj.pre(lowerlimit);
+            upperlimit = obj.pre(upperlimit);
+            centerpoint = (upperlimit + lowerlimit) / 2;
+            demirange = (upperlimit - lowerlimit) / 2;
+            [~, indices_in_range] = KDtree.perform_range_search(obj.root, centerpoint, demirange, 1, obj.dimensions);
+            indices_in_range = obj.post(indices_in_range);
+        end
+
     end
     
-    methods(Static)
+    methods(Static,Access=private)
         
-        function node = create_tree(pointarray, depth, dimensions)
+        function node = create_tree(pointarray, indexarray, depth, dimensions)
             if isempty(pointarray)
                 node = [];
                 return;
             end
-
-            n_points = size(pointarray,1);
-            middle = 1 + floor(n_points / 2);
+            
+            points = size(pointarray,1);
+            middle = 1 + floor(points / 2);
             dimension = mod(depth-1, dimensions) + 1;
-            sortedarray = sortrows(pointarray, dimension);
+            [sortedarray, index] = sortrows(pointarray, dimension);
+            sortedindex = indexarray(index);
 
-            node.left = KDtree.create_tree(sortedarray(1:middle-1,:),depth + 1, dimensions);
-            node.right = KDtree.create_tree(sortedarray(middle+1:n_points,:), depth + 1 , dimensions);
+            node.left = KDtree.create_tree(sortedarray(1:middle-1,:), sortedindex(1:middle-1), depth + 1, dimensions);
+            node.right = KDtree.create_tree(sortedarray(middle+1:points,:), sortedindex(middle+1:points), depth + 1 , dimensions);
             node.point = sortedarray(middle,:);
+            node.index = sortedindex(middle);
         end
         
-        function neighbors = get_neighborhood(node, refpoint, radius2, depth, dimensions)
+        function [points, indices] = get_neighborhood(node, refpoint, radius2, depth, dimensions)
 
             if isempty(node)
-                neighbors = [];
+                points = [];
+                indices = [];
                 return
             end
 
-            if KDtree.distance2(node.point, refpoint) < radius2
-                neighbors = node.point;
+            if KDtree.distance2(node.point, refpoint) <= radius2
+                points = node.point;
+                indices = node.index;
             else
-                neighbors = [];
+                points = [];
+                indices = [];
             end
 
             dimension = mod(depth-1, dimensions) + 1;
@@ -67,63 +115,106 @@ classdef KDtree
                 secondary = node.left;
             end
 
-            neighbors_primary = KDtree.get_neighborhood(primary, refpoint, radius2, depth+1, dimensions);
-            neighbors = [neighbors ; neighbors_primary];
+            [points_primary, indices_primary] = KDtree.get_neighborhood(primary, refpoint, radius2, depth+1, dimensions);
+            points = [points ; points_primary];
+            indices = [indices ; indices_primary];
 
             if delta^2 < radius2
-                neighbors_secondary = KDtree.get_neighborhood(secondary, refpoint, radius2, depth+1, dimensions);
-                neighbors = [neighbors ; neighbors_secondary];
+                [points_secondary, indices_secondary] = KDtree.get_neighborhood(secondary, refpoint, radius2, depth+1, dimensions);
+                points = [points ; points_secondary];
+                indices = [indices ; indices_secondary]; 
             end
         end
         
-        function [nearestneighbor,smallestdistance] = find_nearest_neighbor(node , refpoint, depth, dimensions, nearestneighbor, smallestdistance  )
+        
+        function [nearestpoint, nearestindex, smallestdistance] = find_nearest_node(node , refpoint, depth, dimensions, nearestpoint, nearestindex, smallestdistance  )
             
             if isempty(node)
-                nearestneighbor = [];
+                nearestpoint = [];
+                nearestindex = -1;
+                smallestdistance = inf;
+                return
+            end
+            
+            distance = KDtree.distance2(node.point, refpoint);
+            if distance < smallestdistance
+                nearestpoint = node.point;
+                nearestindex = node.index;
+                smallestdistance = distance;
+            end
+            
+            dimension = mod(depth-1, dimensions) + 1;
+            delta = node.point(dimension) - refpoint(dimension);
+            if delta > 0
+                primary = node.left;
+                secondary = node.right;
+            else
+                primary = node.right;
+                secondary = node.left;
+            end
+            
+            [point_primary, index_primary, distance_primary] = KDtree.find_nearest_node(primary, refpoint, depth+1, dimensions, nearestpoint, nearestindex, smallestdistance);
+            if distance_primary < smallestdistance
+                nearestpoint = point_primary;
+                nearestindex = index_primary;
+                smallestdistance = distance_primary;
+            end
+            if delta^2 < smallestdistance
+                [point_secondary, index_secondary, distance_secondary] = KDtree.find_nearest_node(secondary, refpoint, depth+1, dimensions, nearestpoint, nearestindex, smallestdistance);
+                if distance_secondary < smallestdistance
+                    nearestpoint = point_secondary;
+                    nearestindex = index_secondary;
+                    smallestdistance = distance_secondary;
+                end
+            end
+        end
+
+
+        function [points_in_range, indices_in_range] = perform_range_search(node, centerpoint, demirange, depth, dimensions)
+
+            if isempty(node)
+                points_in_range = [];
+                indices_in_range = [];
                 return
             end
 
-            distance = KDtree.distance2(node.point, refpoint);
-            if distance < smallestdistance
-                smallestdistance = distance;
-                nearestneighbor = node.point;
+            if KDtree.inside(node.point, centerpoint, demirange)
+                points_in_range = node.point;
+                indices_in_range = node.index;
+            else
+                points_in_range = [];
+                indices_in_range = [];
             end
 
             dimension = mod(depth-1, dimensions) + 1;
-            delta = node.point(dimension) - refpoint(dimension);
-            if delta > 0 
+            delta = node.point(dimension) - centerpoint(dimension);
+            
+            if delta > 0
                 primary = node.left;
-                secondary = node.right; 
+                secondary = node.right;
             else
                 primary = node.right;
                 secondary = node.left;
             end
 
-            neighbor_primary = KDtree.find_nearest_neighbor(primary, refpoint, depth+1, dimensions, nearestneighbor, smallestdistance);
-            if ~isempty(neighbor_primary)
-                primarydistance = KDtree.distance2(neighbor_primary, refpoint);
-                if primarydistance < smallestdistance
-                    smallestdistance = primarydistance;
-                    nearestneighbor = neighbor_primary;
-                end
-                if delta^2 < smallestdistance
-                    neighbor_secondary = KDtree.find_nearest_neighbor(secondary, refpoint, depth+1, dimensions, nearestneighbor, smallestdistance);
-                    if ~isempty(neighbor_secondary)
-                        secondarydistance = KDtree.distance2(neighbor_secondary, refpoint);
-                        if secondarydistance < smallestdistance
-                            smallestdistance = secondarydistance;
-                            nearestneighbor = neighbor_secondary;
-                        end
-                    end
-                end
+            [points_primary, indices_primary] = KDtree.perform_range_search(primary, centerpoint, demirange, depth+1, dimensions);
+            points_in_range = [points_in_range; points_primary];
+            indices_in_range = [indices_in_range; indices_primary];
+
+            if abs(delta) < demirange(dimension)
+                [points_secondary, indices_secondary] = KDtree.perform_range_search(secondary, centerpoint, demirange, depth+1, dimensions);
+                points_in_range = [points_in_range ; points_secondary];
+                indices_in_range = [indices_in_range; indices_secondary];
             end
         end
-
 
         function d = distance2(p1,p2)
             d = sum((p2-p1).^2);
         end
 
+        function b = inside(p, centerpoint, halfrange)
+            b = all(p <= centerpoint + halfrange & p >= centerpoint - halfrange);
+        end
         
     end
 end
--- kddemo.m
+++ kddemo.m
@@ -1,25 +1,25 @@
 function kddemo()
 
+% Neighborhood search
 xy = [rand(1000,1) rand(1000,1)];
 kd = KDtree(xy);
 r = 0.2;
 p = [0.27 0.66];
 nb = kd.neighborhood(p,r);
-
-figure
+figure('numbertitle','off')
 plot(xy(:,1),xy(:,2),'b.')
 hold on
 plot(nb(:,1),nb(:,2),'ro')
 viscircles(gca,p,r,'color',[0.5 0.5 0.5],'linestyle','--');
 title('k-d tree neighborhood search')
+axis([0 1 0 1])
 
-
+% Nearest neighbor search
 xy = [rand(10,1) rand(10,1)];
 kd = KDtree(xy);
 p = [0.5 0.5];
 nn = kd.nearestneighbor(p);
-
-figure
+figure('numbertitle','off')
 plot(xy(:,1),xy(:,2),'b.')
 hold on
 plot(nn(1),nn(2),'ro')
@@ -27,4 +27,19 @@ plot(p(1),p(2),'k*')
 r = sqrt((nn(1)-p(1))^2 + (nn(2)-p(2))^2);
 viscircles(gca,p,r,'color',[0.5 0.5 0.5],'linestyle','--');
 plot([p(1) nn(1)],[p(2) nn(2)],'k')
-title('k-d nearest neighbor search')
+title('k-d nearest neighbor search') 
+axis([0 1 0 1])
+
+% Range search using indices
+xy = [rand(1000,1) rand(1000,1)];
+kd = KDtree(xy);
+lower = [0.44 0.24];
+upper = [0.68 0.72];
+ix = kd.rangesearch_indices(lower,upper);
+figure('numbertitle','off')
+plot(xy(:,1),xy(:,2),'b.')
+hold on
+plot(xy(ix,1),xy(ix,2),'ro')
+plot([lower(1) upper(1) upper(1) lower(1) lower(1)],[lower(2) lower(2) upper(2) upper(2) lower(2)],'color',[0.5 0.5 0.5],'linestyle','--','linewidth',1.5)
+title('k-d tree range search')
+axis([0 1 0 1])
