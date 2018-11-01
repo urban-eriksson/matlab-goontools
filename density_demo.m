a5078a12c97c8547894a71014acb95002373bd0d
Urban Eriksson <123ube@gmail.com>
2018-11-01 22:36:18 +0100

    gdensity
    gdensity has been aligned with k-d tree

--- density_demo.m
+++ density_demo.m
@@ -1,14 +1,38 @@
 function density_demo()
 figure(2001)
 clf
-data = rand(1,1000);
-%data = [0.5];
-hist(data,20);
+data = [1 3 4 4.5 5 5 8 8.2 9];
+delta = 0.1;
+bins = -1:delta:11;
+dens = gdensity(data,bins,0.3);
+disp(['Integration of the density ideally equals the number of points : ' num2str(sum(dens * delta))])
+plot(bins,dens*delta,'b','linewidth',1)
 hold on
-bins = -0.2:0.02:1.2;
-tic
-dens = gdensity(data,bins,0.05);
-toc
-sum(dens)
-plot(bins,dens,'r','linewidth',1.5)
+plot(data,zeros(size(data)),'r.','markersize',20)
+
+xlabel('data')
+ylabel('density')
+title('1D density')
+
+figure(2002)
+clf
+mu = [4 5];
+sigma = [3 1.5; 1.5 1];
+data = mvnrnd(mu,sigma,100);
+plot(data(:,1),data(:,2),'+')
+axis([0 10 0 10])
+xlabel('x')
+ylabel('y')
+title('Multivariate Gaussian')
+
+figure(2003)
+clf
+[xbin,ybin]=meshgrid(0:0.1:10,0:0.1:10);
+bins = [xbin(:) ybin(:)];
+dens = gdensity(data,bins,0.3);
+mesh(xbin,ybin,reshape(dens,size(xbin)));
+xlabel('x')
+ylabel('y')
+title('2D density of multivariate Gaussian')
+
 
--- gdensity.m
+++ gdensity.m
@@ -1,21 +1,30 @@
-function dens = gdensity(data, bins, sigma, reach)
+function dens = gdensity_new(data, bins, sigma, reach)
 %GDENSITY Gaussian density
 
 if nargin < 4
     reach = 3 * sigma;
 end
 
-data_sorted = sort(data);
-dens = zeros(size(bins));
-sigma2 = sigma^2;
-Ndata = length(data);
-Nbins = length(bins);
-for j = 1:Nbins
-    [idx_low, idx_high] = findIndexRange(bins(j) - reach, bins(j) + reach, data_sorted);
-    for k = idx_low:idx_high
-        dens(j) = dens(j) + exp(-0.5 * (data_sorted(k) - bins(j))^2 / sigma2);
-    end
+if size(data,2) > size(data,1)
+    transpose = true;
+    data = data';
+    bins = bins';
+else
+    transpose = false;
 end
 
-dens = 1/(sqrt(2*pi) * sigma * Ndata) * dens;
+kd = KDtree(data);
+sigma2 = sigma^2;
+Nbins = size(bins,1);
+dens = zeros(Nbins,1);
 
+for j = 1:Nbins
+    indices = kd.neighborhood_indices(bins(j,:),reach);
+    dens(j) = sum( exp(-0.5 * sum( (data(indices,:) - bins(j,:)).^2 / sigma2, 2) ) );
+end
+
+dens = 1/(sqrt(2*pi) * sigma) * dens;
+
+if transpose
+    dens = dens';
+end
